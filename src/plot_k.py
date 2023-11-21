a764403148c2d1081989aecec713f9e65f9da39d
Fiona Young <fiona.young.15@ucl.ac.uk>
2023-11-21 18:15:05 +0000

    ✍🏻 Address Jon's comments chaps 3-6 / make notes for addressing later
    - typos
    - Add nonlinear vs affine experiment
    - Add registration chapter summary
    - Make final version of k_exp figure and clarify caption
    - Give details on dataset acquision params for HCP and traning data, add 
    age ranges
    - Remove duplicate paragraph about ground truth comparisons

--- src/plot_k.py
+++ src/plot_k.py
@@ -4,7 +4,7 @@ from scipy.special import lambertw
 from os import path, pardir
 
 S = 0
-T = 5
+T = 4
 B = 15
 P = np.arange(0,25,0.2)
 
@@ -13,6 +13,7 @@ def k(x, l):
   return (1-c) * np.exp(-l*abs((x)/(B-S))) + c
 
 plt.rcParams['text.usetex'] = True
+# plt.rcParams['font.size'] = 13
 
 l = 8
 lmax = lambertw(-((B-S)/(T-S))*np.exp(-((B-S)/(T-S))), k=0).real + (B-S)/(T-S)
@@ -25,15 +26,18 @@ lin_maxl = 1 - abs((P-S)/(T-S))
 
 y = [0,100]
 
-fig, ax = plt.subplots(figsize=(5,3), layout='tight')
+fig, ax = plt.subplots(figsize=(4.5,3), layout='tight')
 
-plotwidth=0.8
-ax.plot(P-S,k_,linewidth=plotwidth, label=f'$k_{{l={l:d}}}$')
-ax.plot(P-S,k_lmax,linewidth=plotwidth, label=f'$k_{{l=l_{{max}}}}$')
-ax.plot(P-S,lin,linewidth=plotwidth, label=r'$k=1-\frac{{D_P}}{{D_b}}$')
-ax.plot(P-S,lin_maxl,linewidth=plotwidth, label=r'$k=1-\frac{{D_P}}{{D_t}}$')
+# Indicate tumour and brain boundaries
+# ax.plot([S+B,S+B], y, 'k--', [S-B,S-B], y, 'k--', [S+T,S+T], y, 'k:', [S-T,S-T], y, 'k:' )
+ax.axvspan(S, T, facecolor='k', alpha=0.1)
+ax.axvspan(S, B, facecolor='k', alpha=0.1)
 
-ax.plot([S+B,S+B], y, 'k--', [S-B,S-B], y, 'k--', [S+T,S+T], y, 'k:', [S-T,S-T], y, 'k:' )
+plotwidth=0.6
+# ax.plot(P-S,k_,linewidth=plotwidth, label=f'$k_{{l={l:d}}}$')
+ax.plot(P-S,k_lmax, 'k-', linewidth=plotwidth, label=f'$k_{{l=l_{{max}}}}$')
+ax.plot(P-S,lin_maxl, 'k--', linewidth=plotwidth, label=r'$k=1-\frac{{D_P}}{{D_t}}$')
+ax.plot(P-S,lin, 'k:', linewidth=plotwidth, label=r'$k=1-\frac{{D_P}}{{D_b}}$')
 
 ax.set_ylim([0,1.1])
 ax.set_yticks([])
@@ -43,7 +47,8 @@ ax.set_xticklabels(['$D_t$', '$D_b$'])
 ax.set_ylabel('$k$', fontsize=15)
 ax.set_xlabel(r'$D_p$', fontsize=15)
 
-leg = plt.legend(loc='upper right', fancybox=False, edgecolor='k', framealpha=1)
+leg = plt.legend(loc='upper center',  bbox_to_anchor=(T+(B-T)/2, 1),bbox_transform=ax.transData,
+                  frameon=False, fontsize='medium')
 # leg.legendPatch.set(linestyle='--')
 
-plt.savefig(path.join(path.dirname(__file__), 'draft_figs', 'chapter_3', 'k.png'), dpi=160)
+plt.savefig(path.join(path.dirname(__file__), 'draft_figs', 'chapter_3', 'k.png'), dpi=300)
