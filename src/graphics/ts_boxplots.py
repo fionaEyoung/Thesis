bb11794a325a179e73e36f6e44286337431c6e1e
Fiona Young <fiona.young.15@ucl.ac.uk>
2023-12-30 16:58:26 +0000

    🖼️ Complete big TS comparison scatter plot

--- src/graphics/ts_boxplots.py
+++ src/graphics/ts_boxplots.py
@@ -106,6 +106,56 @@ def main():
   all_metrics_fig.savefig(path.join(results_dir, f'{filename}.pdf'),
               transparent=False, dpi=80)#, bbox_inches='tight', pad_inches=0.05)
 
+  ## PART 2: giant scatter plot
+
+  filename = 'ts_scatter'
+  include_metrics = ['binary_dice','density_correlation']
+  datasets = ['hcp_test_all_tracts_nc', 'hcp_test_all_tracts_c']
+  with open(path.join(data_dir, 'ts_tracts.txt')) as f:
+      TRACTS = [t.lower().rpartition('_')[0] if (('left' in t) or ('right' in t)) else t.lower() for t in f.read().strip().split('\n') ]
+  all_results = pd.DataFrame()
+  for dataset in datasets:
+    datafile = f'{dataset}.pkl'
+    all_results = pd.concat((all_results, pd.read_pickle(path.join(data_dir, datafile))))
+
+  n = len(METHODS)
+  n_m = len(include_metrics) # nr of metrics
+  n_c = n-1 # nr of comparisons
+  n_t = len(TRACTS) # nr of tracts
+
+  big_fig, bix_fig_ax = plt.subplots(nrows=1,
+                                    ncols=len(include_metrics),
+                                    figsize=set_size(ratio=1.2),
+                                    layout='constrained')
+
+  for metric, ax in zip(include_metrics, bix_fig_ax):
+
+    mask = (all_results['methods'] == {compare_against, 'TF'})
+    # print(all_results[mask].columns)
+    sorted_index = all_results[mask].groupby(['tract','hem']).mean(numeric_only=True)[metric].sort_values().index
+    tick_labels = [' '.join(s).upper().strip('_') for s in sorted_index.tolist()]
+
+    for i, method in enumerate(order):
+      mask = (all_results['methods'] == {compare_against, method})
+      vals = all_results[mask].groupby(['tract','hem']).mean(numeric_only=True)[metric][sorted_index]
+
+      ax.plot(vals, tick_labels, METHOD_SHAPES[method], color=METHOD_PROPS[method]['color'], label=method, ms=4)
+
+      ax.minorticks_on()
+      ax.grid(which="major", axis='y', color='#D2D2D2', linestyle=':')
+      ax.yaxis.tick_right()
+      ax.tick_params(axis='y', which='minor', right=False, left=False)
+      ax.set_ylim([-1,len(TRACTS)])
+      ax.grid(which="minor", axis='x', color='#D2D2D2', linestyle='-')
+      ax.grid(which="major", axis='x', color='#D2D2D2', linestyle='-', lw=1.8)
+      ax.set_xlim([0.1, 1])
+      ax.set_xlabel(METRIC_PARAMS[metric]['title'])
+
+  bix_fig_ax[0].legend()
+  # plt.tight_layout()
+  plt.margins(0,0)
+  big_fig.savefig(path.join(results_dir, f'{filename}.pdf'),
+              transparent=False, dpi=120)#, bbox_inches="tight", pad_inches=0)
 
 
 if __name__ == '__main__':
